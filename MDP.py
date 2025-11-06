a730dabad2dd0c87bfadf7146208d29f788469c6
Blanch121 <24ablanch@hinghamschools.org>
2025-11-06 11:13:24 -0500

    Wrote doc-strings for all of the functions in the MDP class. Additionally, the feed_photo_loc function now returns itself so multiple feeds can be used sequentially. Finally, the states_to_transition function's name has been changed to better demonstrate it being a private function. TO-DO: extensive testing

--- MDP.py
+++ MDP.py
@@ -7,44 +7,101 @@ ACTIONS = np.array([
 ])
 
 class MDP:
-    """Class that represents a Markov Decision Process (MDP)"""
+    """Class that represents a Markov Decision Process (MDP)."""
 
     def __init__(self, state_grid, delta_l, delta_w, cb):
-        """DO NOT INITIALIZE THROUGH THIS; USE BUILDER"""
+        """
+        Private constructor for the MDP class, instead use builder to create objects.
+
+        Parameters
+        ----------
+        state_grid : 2d numpy array with unknown shape
+                     contains 1 if that space is a state, 0 otherwise.
+        delta_l    : float
+                     length of each grid space
+        delta_w    : float
+                     width of each grid space
+        cb         : 2x1 numpy vector
+                     coordinates of vertex being used as the new origin
+        """
         self.state_grid = state_grid
-        self.transition_matrix = MDP.states_to_transition(state_grid)
+        self.transition_matrix = MDP.__states_to_transition(state_grid)
         self.delta_l = delta_l
         self.delta_w = delta_w
         self.cb = cb
 
 
     class MDP_builder:
-        """Builder class for MDP class"""
+        """Builder class for MDP class."""
         def __init__(self, cb, cl, cw, l_segments, w_segments):
-            """Creates a builder object using vectors for three points and the amount of segments desired"""
+            """
+            Creates a builder object using vectors for three points and the amount of segments desired.
+
+            Parameters
+            ----------
+            cb         : 2x1 numpy vector
+                         coordinate of vertex being used as the new origin
+            cl         : 2x1 numpy vector
+                         coordinate of vertex defining the length from the origin
+            cw         : 2x1 numpy vector
+                         coordinate of vertex defining the width from the origin
+            l_segments : int
+                         amount of segments to split length into
+            w_segments : int
+                         amount of segments to split width into
+            """
             self.cb = cb
             self.delta_l = (cl - cb) / l_segments
             self.delta_w = (cw - cb) / w_segments
             self.grid = np.empty((l_segments, w_segments), dtype=bool)
 
         def feed_photo_loc(self, coord):
-            """Feeds a photo's coordinates into the mdp; filling in that state if it did not exist before"""
+            """
+            Feeds a photo's coordinates into the mdp; filling in that state if it did not exist before.
+
+            Parameters
+            ----------
+            coord : 2x1 numpy vector
+                    Coordinate of the photo to be fed into the mdp
+
+            Returns
+            -------
+            MDP-Builder
+                Current MDP-builder object
+            """
             c = coord - self.cb
             pr_w = np.dot(c, self.delta_w) / np.linalg.norm(self.delta_w)
             pr_l = np.dot(c, self.delta_l) / np.linalg.norm(self.delta_l)
             x = int(pr_w)
             y = int(pr_l)
             self.grid[y, x] = 1
+            return self
 
         def create(self):
-            """Creates MDP object from current builder"""
+            """
+            Creates MDP object from current builder.
+
+            Returns
+            -------
+            MDP
+                MDP object created from the current builder
+            """
             return MDP(self.grid, self.delta_l, self.delta_w, self.cb)
 
-    #turns a state grid (which is a matrix of bools that represent the whether a grid piece is a state or not)
-    #into a transition matrix
-    def states_to_transition(state_grid):
-        """turns a state grid (which is a matrix of bools that represent the whether a grid piece is a state or not)
-            into a transition matrix"""
+    def __states_to_transition(state_grid):
+        """
+        Turns a state grid (matrix of bools that represent if a grid piece is a state) into a transition matrix
+
+        Parameters
+        ----------
+        state_grid : 2d numpy array with unknown shape
+                     Matrix containing the states of the mdp with 1 being a grid having a state, 0 otherwise
+
+        Returns
+        -------
+        3d numpy array
+            Transition matrix formatted [action][state][new state]
+        """
         num_states = np.sum(state_grid)
         transition_matrix = np.zeros((len(ACTIONS), num_states, num_states), dtype=bool)
         curr_state = 0
@@ -64,7 +121,19 @@ class MDP:
         return transition_matrix
 
     def coord_to_state(self, coord):
-        """Converts the latitude and latitude in a 2x1 np vector into the index of its state"""
+        """
+        Converts the latitude and latitude in a 2x1 np vector into the index of its state.
+
+        Parameters
+        ----------
+        coord : 2x1 numpy vector
+                Coordinate of the point to be converted to a state's index
+
+        Returns
+        -------
+        int
+            The index of the state at that coordinate
+        """
         c = coord - self.cb
         pr_w = np.dot(c, self.delta_w) / np.linalg.norm(self.delta_w)
         pr_l = np.dot(c, self.delta_l) / np.linalg.norm(self.delta_l)
@@ -73,7 +142,19 @@ class MDP:
         return np.sum(self.state_grid[:y])+np.sum(self.state_grid[y][:x])
 
     def state_to_coord(self, state):
-        """Converts a states index to its coordinates in latitude and longitude as a 2x1 np vector"""
+        """
+        Converts a states index to its coordinates in latitude and longitude as a 2x1 np vector.
+
+        Parameters
+        ----------
+        state : int
+                Index of the state to convert to the bottom left coordinate
+
+        Returns
+        -------
+        2x1 numpy vector
+            The coordinates of the bottom left vertex of that state
+        """
         states_left = state
         for y in range(len(state)):
             for x in range(len(state[0])):
@@ -83,8 +164,21 @@ class MDP:
 
 
     def calc_transition_prob(self, old_state, action, new_state):
-        """Calculates the T(s, a, s') value for a specific state, action, state tuple"""
+        """
+        Calculates the T(s, a, s') value for a specific state, action, state tuple.
+
+        Parameters
+        ----------
+        old_state : int
+                    Index of the state starting at
+        action    : int
+                    Index of the action taken at that state
+        new_state : int
+                    Index of the new state reached by the action
+
+        Returns
+        -------
+        float
+            The probability of transitioning from old_state to new_state with the given action
+        """
         return self.transition_matrix[action][old_state][new_state]
-
-
-
